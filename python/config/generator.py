c2ba85ede37897b96a83053dd14521cdaf1afad6
Graham Hough <graham.hough117@gmail.com>
2016-05-13 19:49:41 -0400

    generator update

--- python/config/generator.py
+++ python/config/generator.py
@@ -1,24 +1,24 @@
 def main():
   output = ''
 
-  firstName = input("What is your first name?")
-  lastName = input("What is your last name?")
-  email = input("What is your email?")
-  twitter = input("What is your twitter @?")
-  customDomain = input("Are you using a custom domain? No or domain name.")
+  firstName = input("What is your first name: ")
+  lastName = input("What is your last name: ")
+  email = input("What is your email: ")
+  twitter = input("What is your twitter @: ")
+  customDomain = input("Are you using a custom domain \(string or \"No\"\): ")
   theme = "Default"
 
-  output += str("FirstName:\n\t" + firstName + "\n")
-  output += str("LastName:\n\t" + lastName + "\n")
-  output += str("Email:\n\t" + email + "\n")
-  output += str("twitter:\n\t" + twitter + "\n")
+  output += str("FirstName:\n\t" + firstName + "\n\n")
+  output += str("LastName:\n\t" + lastName + "\n\n")
+  output += str("Email:\n\t" + email + "\n\n")
+  output += str("twitter:\n\t" + twitter + "\n\n")
   if customDomain == "n" or customDomain == "No" or customDomain == "N":
-    output += str("Domain:\n\tFalse\n")
+    output += str("Domain:\n\tFalse\n\n")
   else:
-    output += str("Domain:\n\t" + customDomain + "\n")
-  output += str("Theme:\n\t" + theme + "\n")
+    output += str("Domain:\n\t" + customDomain + "\n\n")
+  output += str("Theme:\n\t" + theme + "\n\n")
 
-  f = open('../config', 'w')
+  f = open('../../config.yml', 'w')
   f.write(output)
 
 
