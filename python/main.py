886c68d4cd7df0e119511803406ef08e132615a8
Graham Hough <graham.hough117@gmail.com>
2016-05-16 17:47:25 -0400

    removed author and cleaned up main.py

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 import sys
 from builder import *
+from post_builder import *
 
 #For refrence, line 6 lists the number of arguments by calling for the length of the arguemnt list.
 #Line 7 prints the list of objects as a refrenceable string.
@@ -35,46 +36,28 @@ def help():
     """)
   sys.exit()
 
-if len(sys.argv) == 2:
-  if str(sys.argv[1]).title() == "Help":
-    print(help())
-  if str(sys.argv[1]).title() == "H":
-    print(help())
-
-elif len(sys.argv) == 3:
-  if str(sys.argv[1]).title() != "Post" and str(sys.argv[1]).title() != "New":
-    print(help())
+def findCallType(argument):
+  argument.pop(0)
+  if len(argument) == 2:
+    if argument[0].title() == 'New':
+      newSite = Site(argument[1])
+      newSite.directoryDraw()
+      newSite.config()
+      newSite.configDraw()
+    elif argument[0].title() == 'Post':
+      newPost = Post(argument[1])
+    else:
+      print(help())
+  elif len(arguments) == 3:
+      if argument[2] == "--no-config":
+        newSite = Site(argument[1])
+        newSite.directoryDraw()
+        newSite.noConfigDraw()
+        newSite.configDraw()
+        print("Please fill out config.yml in your sites directory")
   else:
-    arg1 = str(sys.argv[1])
-
-  arg2 = str(sys.argv[2])
-
-elif len(sys.argv) == 4:
-  if str(sys.argv[1]).title() != "Post" and str(sys.argv[1]).title() != "New":
     print(help())
-  else:
-    arg1 = str(sys.argv[1])
 
-  arg2 = str(sys.argv[2])
-  if str(sys.argv[3]) == "--no-config":
-    arg3 = str(sys.argv[3])
-  else:
-    print("Invalid flag option")
-    print(help())
-else:
-  print("Incorrect arguments, please refer to the docs.")
-  print(help())
 
-if 'arg1' in locals() and 'arg2' in locals() and arg1 == "new":
-  projectTitle = arg2
-  print('New blog will be generated as ' + str(arg2))
-  #Site's self.name is passed below.
-  newSite = Site(str(arg2))
-  newSite.directoryDraw()
-  if 'arg3' in locals() and arg3 == '--no-config':
-    print("Please fill out config.yml in your sites directory")
-    newSite.noConfigDraw()
-    newSite.configDraw()
-  else:
-    newSite.config()
-    newSite.configDraw()
+argument = sys.argv
+findCallType(argument)
--- python/post_builder.py
+++ python/post_builder.py
@@ -4,7 +4,7 @@ import time
 class Post():
   """A post generator for Pyder, a static blog generator"""
   
-  def __init__(self, title, author):
+  def __init__(self, title):
     """Initialize attributes on a post-to-post basis. This consists of generating a dir with the date, then the name of the post."""
     self.title = title
     self.fileTitle = self.title.lower()[0:15].replace(" ", "_")
@@ -14,21 +14,17 @@ class Post():
 
     self.genDate = str(time.strftime("%d.%m.%Y"))
 
-    self.author = author.title()
-
     self.postsMadeToday = os.path.exists(os.getcwd() + '/posts/' + self.genDate)
 
     if self.postsMadeToday == True:
       f = open(('posts/' + self.genDate + '/' + self.fileTitle + '.md'), 'w')
       content = ''
       content += str("#" + self.title + "\n\n")
-      content += str("###### By " + self.author + "\n\n")
       f.write(content)
     else:
       os.mkdir('posts/' + self.genDate)
       f = open(('posts/' + self.genDate + '/' + self.fileTitle + '.md'), 'w')
       content = ''
       content += str("#" + self.title + "\n\n")
-      content += str("###### By " + self.author + "\n\n")
       f.write(content)
 
